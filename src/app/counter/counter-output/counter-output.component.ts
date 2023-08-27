cee7ad3d8bdf925c1990c5c0b2e3b8bb3fc14103
Willian <williamhernandez450@gmail.com>
2023-08-26 19:28:39 -0600

    Modified replace operator rxjs Subscription with Observable

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,34 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterState } from '../state/counter.state';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.scss']
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent implements OnInit {
 
   counter?: number;
-  counterSubscription!: Subscription;
+  counter$!: Observable<{counter: number}>;
 
   constructor(
     private store: Store<{ counter: CounterState }>
-  ) { }
-
-  ngOnInit(): void {
-    this.counterSubscription = this.store
-      .select('counter')
-      .subscribe(data => {
-        this.counter = data.counter;
-    });
+  ) {
   }
 
-  ngOnDestroy(): void {
-    if ( this.counterSubscription ) {
-      this.counterSubscription.unsubscribe();
-    }
+  ngOnInit(): void {
+    this.counter$ = this.store.select('counter');
   }
 
 }
