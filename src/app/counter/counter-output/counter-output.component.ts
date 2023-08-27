7eba097f362e85392f81cd38396b6dac6cfe48da
Willian <williamhernandez450@gmail.com>
2023-08-26 19:15:03 -0600

    Add interface and method with operator rxjs Subscription

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,23 +1,34 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.scss']
 })
-export class CounterOutputComponent implements OnInit {
+export class CounterOutputComponent implements OnInit, OnDestroy {
 
   counter?: number;
+  counterSubscription!: Subscription;
 
   constructor(
-    private store: Store<{ counter: {counter: number} }>
+    private store: Store<{ counter: CounterState }>
   ) { }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data => {
-      this.counter = data.counter;
-    })
+    this.counterSubscription = this.store
+      .select('counter')
+      .subscribe(data => {
+        this.counter = data.counter;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if ( this.counterSubscription ) {
+      this.counterSubscription.unsubscribe();
+    }
   }
 
 }
